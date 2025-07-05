edf28cd1a4f72a4dc6e2369fb1675cd8e8062ab4
Adrian Klingen <adrian.klingen@deptagency.com>
2025-07-05 11:21:40 +0200

    Move Raid CDs to separate sections

--- ui/core/constants/tooltips.ts
+++ ui/core/constants/tooltips.ts
@@ -1,4 +1,6 @@
-export const BUFFS_SECTION = 'Buffs provided by other party/raid members.';
+export const BUFFS_SECTION = 'Buffs provided by <strong>other</strong> party/raid members.';
+export const EXTERNAL_DAMAGE_COOLDOWN_SECTION = 'Major damage cooldowns provided by <strong>other</strong> party/raid members.';
+export const EXTERNAL_DEFENSIVE_COOLDOWN_SECTION = 'Major defensive cooldowns provided by <strong>other</strong> party/raid members.';
 export const DEBUFFS_SECTION = 'Debuffs applied by other raid members.';
 export const COOLDOWNS_SECTION =
 	'Specify cooldown timings, in seconds. Cooldowns will be used as soon as possible after their specified timings. When not specified, cooldowns will be used when ready and it is sensible to do so.<br><br>Multiple timings can be provided by separating with commas. Any cooldown usages after the last provided timing will use the default logic.';
