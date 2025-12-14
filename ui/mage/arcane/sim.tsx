7b44a15af28e5ca034f7f88d42bb3ebe554813b2
Saji <hoaraucg@gmail.com>
2025-12-14 15:41:38 -0500

    Add reforge optimizer setting for preferring a reforge to hit instead of to expertise when both options are present. Enable this by default for all mage specs

--- ui/mage/arcane/sim.tsx
+++ ui/mage/arcane/sim.tsx
@@ -172,6 +172,7 @@ export class ArcaneMageSimUI extends IndividualSimUI<Spec.SpecArcaneMage> {
 		this.reforger = new ReforgeOptimizer(this, {
 			statSelectionPresets: statSelectionPresets,
 			enableBreakpointLimits: true,
+			preferHitOverExpertise: true,
 			getEPDefaults: player => {
 				const avgIlvl = player.getGear().getAverageItemLevel(false);
 				if (avgIlvl >= 525) {
