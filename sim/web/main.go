6b156e738705e2982dd802d717597c7703ecff48
Adrian Klingen <adrian.klingen@deptagency.com>
2026-06-09 13:04:24 +0200

    Split async progress from main & add optimized candidates mutex

--- sim/web/main.go
+++ sim/web/main.go
@@ -14,11 +14,9 @@ import (
 	"runtime/pprof"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"syscall"
 	"time"
 
-	uuid "github.com/google/uuid"
 	"github.com/pkg/browser"
 	dist "github.com/wowsims/mop/binary_dist"
 	"github.com/wowsims/mop/sim"
@@ -173,63 +171,6 @@ type asyncAPIHandler struct {
 	handle func(googleProto.Message, chan *proto.ProgressMetrics, string)
 }
 
-type asyncProgress struct {
-	id             string
-	latestProgress atomic.Value
-	pendingMu      sync.Mutex
-	// Buffered partial bulk reforge candidates that have not yet been delivered
-	// to an asyncProgress poll response.
-	pendingOptimizedCandidates []*proto.BulkGearCandidate
-	pendingCandidateIndices    map[int32]struct{}
-}
-
-func (s *server) addNewSim() *asyncProgress {
-	newID := uuid.NewString()
-	simProgress := &asyncProgress{
-		id:                      newID,
-		pendingCandidateIndices: make(map[int32]struct{}),
-	}
-	simProgress.latestProgress.Store(&proto.ProgressMetrics{})
-
-	s.progMut.Lock()
-	s.asyncProgresses[newID] = simProgress
-	s.progMut.Unlock()
-
-	return simProgress
-}
-
-func (p *asyncProgress) appendPendingOptimizedCandidates(candidates []*proto.BulkGearCandidate) {
-	if len(candidates) == 0 {
-		return
-	}
-
-	p.pendingMu.Lock()
-	defer p.pendingMu.Unlock()
-	for _, candidate := range candidates {
-		if candidate == nil || candidate.Gear == nil {
-			continue
-		}
-		if _, exists := p.pendingCandidateIndices[candidate.Index]; exists {
-			continue
-		}
-		p.pendingCandidateIndices[candidate.Index] = struct{}{}
-		p.pendingOptimizedCandidates = append(p.pendingOptimizedCandidates, candidate)
-	}
-}
-
-func (p *asyncProgress) takePendingOptimizedCandidates() []*proto.BulkGearCandidate {
-	p.pendingMu.Lock()
-	defer p.pendingMu.Unlock()
-	if len(p.pendingOptimizedCandidates) == 0 {
-		return nil
-	}
-
-	pending := p.pendingOptimizedCandidates
-	p.pendingOptimizedCandidates = nil
-	p.pendingCandidateIndices = make(map[int32]struct{})
-	return pending
-}
-
 func (s *server) handleAsyncAPI(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -353,6 +294,13 @@ func (s *server) setupAsyncServer() {
 		}
 		latest := progress.latestProgress.Load().(*proto.ProgressMetrics)
 		pendingOptimizedCandidates := progress.takePendingOptimizedCandidates()
+		if latest.FinalBulkSimResult != nil && len(latest.FinalBulkSimResult.OptimizedCandidates) > 0 {
+			remainingOptimizedCandidates := progress.filterUndeliveredOptimizedCandidates(latest.FinalBulkSimResult.OptimizedCandidates)
+			latestWithFilteredFinal := googleProto.Clone(latest).(*proto.ProgressMetrics)
+			latestWithFilteredFinal.FinalBulkSimResult = googleProto.Clone(latest.FinalBulkSimResult).(*proto.BulkSimResult)
+			latestWithFilteredFinal.FinalBulkSimResult.OptimizedCandidates = remainingOptimizedCandidates
+			latest = latestWithFilteredFinal
+		}
 		if len(pendingOptimizedCandidates) > 0 {
 			latestWithPending := googleProto.Clone(latest).(*proto.ProgressMetrics)
 			latestWithPending.OptimizedCandidates = pendingOptimizedCandidates
