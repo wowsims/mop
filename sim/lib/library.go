e2034ab42fe71a19da35bf0b7b924eb249502bec
Adrian Klingen <adrian.klingen@deptagency.com>
2026-06-18 10:23:20 +0200

    Fix database_load

--- sim/lib/library.go
+++ sim/lib/library.go
@@ -120,6 +120,8 @@ func getDatabase(itemIds *int32, numItems int32, enchantIds *int32, numEnchants 
 			SetId:            item.SetID,
 			ScalingOptions:   item.ScalingOptions,
 			ItemEffects:      item.ItemEffects,
+			Unique:           item.Unique,
+			LimitCategory:    item.LimitCategory,
 		}
 	}
 	for i, enchantId := range eids {
