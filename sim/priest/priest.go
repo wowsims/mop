d51b5b83d705a2019f76f114f84a6ce4ad89b275
Johan Hillerström <progr@mmer.nu>
2026-09-05 00:45:28 +0200

    [Priest] Inner Fire increases armor by 60%, not 10%

--- sim/priest/priest.go
+++ sim/priest/priest.go
@@ -79,9 +79,10 @@ func (priest *Priest) AddPartyBuffs(_ *proto.PartyBuffs) {
 }
 
 func (priest *Priest) Initialize() {
+	// Inner Fire (588): +10% spell power and +60% armor.
 	if priest.SelfBuffs.UseInnerFire {
 		priest.MultiplyStat(stats.SpellPower, 1.1)
-		priest.ApplyEquipScaling(stats.Armor, 1.1)
+		priest.ApplyEquipScaling(stats.Armor, 1.6)
 		core.MakePermanent(priest.RegisterAura(core.Aura{
 			Label:    "Inner Fire",
 			ActionID: core.ActionID{SpellID: 588},
